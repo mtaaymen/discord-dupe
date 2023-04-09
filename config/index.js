52d3ebd89323be4efeb8aa0e0a5d74b316210bb4
Hotizana dealer <xaymencraft@gmail.com>
2023-04-09 20:17:16 +0100

    fixed posts and profile scroll

--- config/index.js
+++ config/index.js
@@ -3,6 +3,6 @@
 module.exports = {
     MONGODB_URI: "mongodb+srv://logicielxy:clzNBMLQuowkruXA@discord.naovb9x.mongodb.net/Discord?retryWrites=true&w=majority",
     JWT_SECRET: "Logiciel-Secret",
-    CLIENT_URL: "http://localhost:3000",
+    CLIENT_URL: /*"http://localhost:3000"*/ "https://discord-dupe.netlify.app",
     PORT: 3001,
 }
